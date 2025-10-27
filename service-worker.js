861a1ea4b6a61241d8f877694208fbec261f9544
PipahCookies <theempirez2020@gmail.com>
2025-10-27 20:20:25 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,37 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('pipahcookies-store').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './images/icon-192.png',
-        './images/icon-512.png'
-      ]);
-    })
+// service-worker.js
+const CACHE_NAME = 'pipahcookies-store-v2';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/images/icon1.png',
+  '/images/icon.png'
+];
+
+// Install and cache assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+// Activate and remove old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch from cache first, then network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(res => res || fetch(event.request))
+      .catch(() => caches.match('/index.html'))
   );
 });
